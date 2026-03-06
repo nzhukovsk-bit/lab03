9e78d56d7bf56a7e193107d21c32cca31557645a
nzhukovsk-bit <nzhukovsk@gmail.com>
2026-03-06 13:53:24 +0000

    Add user input

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
 
-using namespace std;   
+using namespace std;                         
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    string name;
+    
+    cout << "Enter your name: ";
+    cin >> name;
+    
+    cout << "Hello world from " << name << endl;
+    
     return 0;
 }
