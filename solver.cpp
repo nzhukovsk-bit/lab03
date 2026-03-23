6bc4f2a8e3ab2d2dad4f0a092746960f90aa1f10
nzhukovsk-bit <nzhukovsk@gmail.com>
2026-03-23 17:51:45 +0000

    Add solver_lib and solver application

---
+++ solver.cpp
@@ -0,0 +1,23 @@
+#include "formatter_ex.h"
+#include "solver.h"
+#include <iostream>
+
+int main() {
+    float a, b, c;
+    
+    std::cout << "Enter coefficients a, b, c: ";
+    std::cin >> a >> b >> c;
+    
+    try {
+        float x1, x2;
+        solve(a, b, c, x1, x2);
+        
+        std::string message = "Roots: " + std::to_string(x1) + " and " + std::to_string(x2);
+        formatter(std::cout, message);
+    }
+    catch (const std::logic_error& e) {
+        formatter(std::cout, e.what());
+    }
+    
+    return 0;
+}
